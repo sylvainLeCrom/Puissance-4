11c118ce5aa57323f71bd33ebc33fd6cbc2e7442
Sylvain LE CROM <sylvain.lecrom@free.fr>
2017-09-29 17:28:28 +0200

    tableau en mode colonnes TESTEJEU

--- TEST JEU/scripts/tests.js
+++ TEST JEU/scripts/tests.js
@@ -1,7 +1,7 @@
 
-//CREATION DE LA GRILLE DU JEU AVEC COORDONNEES EN MODE TABLEAU
+//*CREATION DE LA GRILLE DU JEU AVEC COORDONNEES EN MODE TABLEAU
 
-
+/*
 function creaJeu(lignes,colonnes){
     var lignes = 6;
     var colonnes = 7;
@@ -11,7 +11,7 @@ function creaJeu(lignes,colonnes){
       var tr = document.createElement("TR");
       for (var j = 0; j < colonnes; j++){
         var td = document.createElement("TD");
-        var td_text = eval(i+1)+ ""+ eval(j+1);
+        var td_text = i+ ""+ j;
         td_text = document.createTextNode(td_text);
         td.appendChild(td_text);
         tr.appendChild(td);
@@ -26,24 +26,52 @@ function creaJeu(lignes,colonnes){
     //5) placement du jeu dans la page
     document.getElementById("content").appendChild(plateau);
   }
+*/
 
+//* CREATION DE LA GRILLE DU JEU AVEC COORDONNEES EN MODE COLONNE
 
-
-  function creaJeu2(lignes,colonnes){
+/* function creaJeu(lignes,colonnes){
     var lignes = 6;
     var colonnes = 7;
     // LA GRILLE
     var grille = document.createElement("GRILLE");
-    for(var i = 0; i < lignes; i++){
-      var tr = document.createElement("TR");
-      for (var j = 0; j < colonnes; j++){
-        var td = document.createElement("TD");
-        var td_text = eval(i+1)+ ""+ eval(j+1);
-        td_text = document.createTextNode(td_text);
-        td.appendChild(td_text);
-        tr.appendChild(td);
+    for(var i = 0; i < colonnes; i++){
+      var ul = document.createElement("UL");
+      for (var j = 0; j < lignes; j++){
+        var li = document.createElement("LI");
+        var li_text = i+ ""+ j;
+        li_text = document.createTextNode(li_text);
+        li.appendChild(li_text);
+        ul.appendChild(li);
       }
-      grille.appendChild(tr);
+      grille.appendChild(ul);
+    }
+
+    //4) setting table
+    var plateau = document.createElement("PLATEAU"); 
+    plateau.appendChild(grille);
+     
+    //5) placement du jeu dans la page
+    document.getElementById("content").appendChild(plateau);
+  }
+  */
+
+  function creaJeu(lignes,colonnes){
+    var lignes = 6;
+    var colonnes = 7;
+    var position = 0;
+    // LA GRILLE
+    var grille = document.createElement("GRILLE");
+    for(var i = 0; i < colonnes; i++){
+      var ul = document.createElement("UL");
+      for (var j = 0; j < lignes; j++){
+        var li = document.createElement("LI");
+        var li_text = i+ ""+ j;
+        li_text = document.createTextNode(li_text);
+        li.appendChild(li_text);
+        ul.appendChild(li);
+      }
+      grille.appendChild(ul);
     }
 
     //4) setting table
