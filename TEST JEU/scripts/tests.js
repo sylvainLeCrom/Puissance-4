e2d6a7d2f6470c665c16e62242be6993f3d36350
Sylvain LE CROM <sylvain.lecrom@free.fr>
2017-10-01 01:41:05 +0200

    remplissage colonnes jeuTest

--- TEST JEU/scripts/tests.js
+++ TEST JEU/scripts/tests.js
@@ -1,83 +1,55 @@
 
 //*CREATION DE LA GRILLE DU JEU AVEC COORDONNEES EN MODE TABLEAU
+function creaJeu(lignes, colonnes) {
+  var lignes = 6;
+  var colonnes = 7;
+  var position = 0;
+  // LA GRILLE
+  var grille = document.createElement("GRILLE");
+  for (var i = 0; i < colonnes; i++) {
+    var ul = document.createElement("UL");
+    ul.id = (i + 1);
+    ul.setAttribute("onclick", "posePion()");
+    var li = document.createElement("LI");
+    li.id = "ghost"
+    ul.appendChild(li);
 
-/*
-function creaJeu(lignes,colonnes){
-    var lignes = 6;
-    var colonnes = 7;
-    // LA GRILLE
-    var grille = document.createElement("GRILLE");
-    for(var i = 0; i < lignes; i++){
-      var tr = document.createElement("TR");
-      for (var j = 0; j < colonnes; j++){
-        var td = document.createElement("TD");
-        var td_text = i+ ""+ j;
-        td_text = document.createTextNode(td_text);
-        td.appendChild(td_text);
-        tr.appendChild(td);
-      }
-      grille.appendChild(tr);
+    for (var j = 0; j < lignes; j++) {
+      var li = document.createElement("LI");
+      var li_text = (i + 1) + "" + (j + 1);
+      li.id = li_text;
+      li.className += "vide";
+      li_text = document.createTextNode(li_text);
+      li.appendChild(li_text);
+      ul.appendChild(li);
     }
-
-    //4) setting table
-    var plateau = document.createElement("PLATEAU"); 
-    plateau.appendChild(grille);
-     
-    //5) placement du jeu dans la page
-    document.getElementById("content").appendChild(plateau);
+    grille.appendChild(ul);
   }
-*/
 
-//* CREATION DE LA GRILLE DU JEU AVEC COORDONNEES EN MODE COLONNE
+  //4) setting table
+  var plateau = document.createElement("PLATEAU");
+  plateau.appendChild(grille);
 
-/* function creaJeu(lignes,colonnes){
-    var lignes = 6;
-    var colonnes = 7;
-    // LA GRILLE
-    var grille = document.createElement("GRILLE");
-    for(var i = 0; i < colonnes; i++){
-      var ul = document.createElement("UL");
-      for (var j = 0; j < lignes; j++){
-        var li = document.createElement("LI");
-        var li_text = i+ ""+ j;
-        li_text = document.createTextNode(li_text);
-        li.appendChild(li_text);
-        ul.appendChild(li);
-      }
-      grille.appendChild(ul);
+  //5) placement du jeu dans la page
+  document.getElementById("content").appendChild(plateau);
+}
+
+function posePion() {
+  var couleur = 'jaune';
+  var x = event.target.parentElement.id;
+  var y = 6;
+  
+  while(y>0){
+    var position = x + "" + y;
+    Case = document.getElementById(position);
+    Etat = Case.className;
+    if(Etat == "vide"){
+    document.getElementById(Case.id).className = couleur;
+    return; 
     }
-
-    //4) setting table
-    var plateau = document.createElement("PLATEAU"); 
-    plateau.appendChild(grille);
-     
-    //5) placement du jeu dans la page
-    document.getElementById("content").appendChild(plateau);
-  }
-  */
-
-  function creaJeu(lignes,colonnes){
-    var lignes = 6;
-    var colonnes = 7;
-    var position = 0;
-    // LA GRILLE
-    var grille = document.createElement("GRILLE");
-    for(var i = 0; i < colonnes; i++){
-      var ul = document.createElement("UL");
-      for (var j = 0; j < lignes; j++){
-        var li = document.createElement("LI");
-        var li_text = i+ ""+ j;
-        li_text = document.createTextNode(li_text);
-        li.appendChild(li_text);
-        ul.appendChild(li);
-      }
-      grille.appendChild(ul);
+    else{
+    y=y-1;
     }
-
-    //4) setting table
-    var plateau = document.createElement("PLATEAU"); 
-    plateau.appendChild(grille);
-     
-    //5) placement du jeu dans la page
-    document.getElementById("content").appendChild(plateau);
   }
+  alert("colonne pleine");  
+}
