aaf3592ffec107c8fbfb447d1cd0beb5e1a60a0e
Matthieu Marais <mqtthieu@gmail.com>
2017-10-01 17:51:55 +0200

    test

--- FIREBASE TESTS/app.js
+++ FIREBASE TESTS/app.js
@@ -1,21 +1,26 @@
-(function(){
+(function() {
 
-const config = {
-    apiKey: "AIzaSyCqkzOS8R5k6y6NZ3YKdQdj14iMO7Uvkq8",
-    authDomain: "wcs-puissance-4.firebaseapp.com",
-    databaseURL: "https://wcs-puissance-4.firebaseio.com",
-    projectId: "wcs-puissance-4",
-    storageBucket: "wcs-puissance-4.appspot.com",
-    messagingSenderId: "899774202606"
-  };
-  firebase.initializeApp(config);
-
-
-const preObject = document.getElementById('object');
-
-const dbRefObject = firebase.database().ref().child('object');
-
-dbRefObject.on('value', snap => console.log(snap.val()));
+    const config = {
+        apiKey: "AIzaSyCqkzOS8R5k6y6NZ3YKdQdj14iMO7Uvkq8",
+        authDomain: "wcs-puissance-4.firebaseapp.com",
+        databaseURL: "https://wcs-puissance-4.firebaseio.com",
+        projectId: "wcs-puissance-4",
+        storageBucket: "wcs-puissance-4.appspot.com",
+        messagingSenderId: "899774202606"
+    };
+    firebase.initializeApp(config);
 
 
+    const preObject = document.getElementById('object');
+
+    const dbRefObject = firebase.database().ref().child('object');
+
+    dbRefObject.on('value', snap => console.log(snap.val()));
+
+    var rootRef = firebase.database().ref().child('test');
 }());
+
+function surface() {
+    var surface = document.getElementById("surface1").value;
+    document.write(surface);
+}
