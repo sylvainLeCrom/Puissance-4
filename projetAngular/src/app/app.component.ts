93ee08b87eda116c8657200c9250528f6a712582
Sylvain LE CROM <sylvain.lecrom@free.fr>
2017-10-02 23:24:08 +0200

    chat Firebase

--- projetAngular/src/app/app.component.ts
+++ projetAngular/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  user: Observable<firebase.User>;
+  items: FirebaseListObservable<any[]>;
+  msgVal: string = '';  
+
+  constructor(public afAuth: AngularFireAuth, public af: AngularFireDatabase) {    
+    this.items = af.list('/messages', {
+      query: {
+        limitToFirst: 5,
+        orderByChild : 'reverseDate'
+      }
+    });
+
+    this.user = this.afAuth.authState;
+
+  }
+  login() {
+    this.afAuth.auth.signInAnonymously();
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
+  }
+
+  Send(desc: string) {
+    const date = Date.now();
+    let reverseDate = 0 - date;
+    this.items.push({ reverseDate, message: desc });
+    this.msgVal = '';
+  }
 }
--- projetAngular/src/app/app.module.ts
+++ projetAngular/src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HttpModule } from '@angular/http';
+
+
+import { AngularFireModule } from 'angularfire2';
+// New imports to update based on AngularFire2 version 4
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCqkzOS8R5k6y6NZ3YKdQdj14iMO7Uvkq8",
+      authDomain: "wcs-puissance-4.firebaseapp.com",
+      databaseURL: "https://wcs-puissance-4.firebaseio.com",
+      projectId: "wcs-puissance-4",
+      storageBucket: "wcs-puissance-4.appspot.com",
+      messagingSenderId: "899774202606"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
