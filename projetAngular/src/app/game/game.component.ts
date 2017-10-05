50836f01a4a699797953cf5faa39623d8528c38e
Sylvain LE CROM <sylvain.lecrom@free.fr>
2017-10-05 17:28:06 +0200

    game.comp + identification des UL et LI

--- projetAngular/src/app/game/game.component.ts
+++ projetAngular/src/app/game/game.component.ts
@@ -7,17 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
 
-  public items: any[][];
-  constructor() { 
+  public items: string[][];
+  public couleurJoueur: string;
+  constructor() {
+    this.couleurJoueur = "jaune";
     this.items = [
-        ["vide","vide","vide","vide","vide","ghost"],
-        ["vide","vide","vide","vide","vide","ghost"],
-        ["vide","vide","vide","vide","vide","ghost"],
-        ["vide","vide","vide","vide","vide","ghost"],
-        ["vide","vide","vide","vide","vide","ghost"],
-        ["vide","vide","vide","vide","vide","ghost"],
-        ["vide","vide","vide","vide","vide","ghost"]];
+      ["vide", "vide", "vide", "vide", "vide", "vide","ghost this.couleurJoueur"],
+      ["vide", "vide", "vide", "vide", "vide", "vide", "ghost this.couleurJoueur"],
+      ["vide", "vide", "vide", "vide", "vide", "vide", "ghost this.couleurJoueur"],
+      ["vide", "vide", "vide", "vide", "vide", "vide", "ghost this.couleurJoueur"],
+      ["vide", "vide", "vide", "vide", "vide", "vide", "ghost this.couleurJoueur"],
+      ["vide", "vide", "vide", "vide", "vide", "vide", "ghost this.couleurJoueur"],
+      ["vide", "vide", "vide", "vide", "vide", "vide", "ghost this.couleurJoueur"]
+    ];
+    this.items[4][0] = "jaune";
   }
   ngOnInit() {
+  }
+  onClick(event: Event) {
+    
+   
+    alert("click sur colonne "+ event.target + " marche");
   }
 }
