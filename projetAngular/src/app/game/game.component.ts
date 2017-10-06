b96ffe597f1be02358c4e30711e68eb6b2dfb50f
Matthieu Marais <mqtthieu@gmail.com>
2017-10-06 13:54:29 +0200

    test horizontal

--- projetAngular/src/app/game/game.component.ts
+++ projetAngular/src/app/game/game.component.ts
@@ -29,7 +29,7 @@ export class GameComponent implements OnInit {
   ngOnInit() {
 
   }
-  clickedColumn(id: number) {
+  clickedColumn(id: number): void {
     let x = id;
     let y = 6;
     while (y > 0) {
@@ -37,21 +37,46 @@ export class GameComponent implements OnInit {
         this.grille[x][y] = this.joueurEnCours;
 
         //on test si victoire verticale
+        let xTest = x;
+        let yTest = y;
         let align = 0;
-        while (y <= 6) {
-          if (this.grille[x][y] == this.joueurEnCours) {
-            align = align+1;
-            console.log(align);
+        while (yTest <= (y+3) && yTest <= 6) {
+          if (this.grille[xTest][yTest] == this.joueurEnCours) {
+            align = align + 1;
             if (align == 4) {
               alert(this.joueurEnCours + " gagne Wouhouuuu");
               return;
             };
           } else {
-            return;
+            align = 0;
           };
-          y++;
+          yTest++;
         }
 
+        //on test si victoire horizontale
+         xTest = x;
+         yTest = y;
+         align = 0;
+         let recule = 3;
+ 
+         //on cherche la case de départ du test
+         while (xTest > 0 && recule > 0) {
+           xTest--;
+           recule--;
+         }
+         //on check the lign
+         while (xTest <= (x+3) && xTest <= 6) {
+           if (this.grille[xTest][yTest] == this.joueurEnCours) {
+             align = align + 1;
+             if (align == 4) {
+               alert(this.joueurEnCours + " gagne Wouhouuuu");
+               return;
+             };
+           } else {
+             align = 0;
+           };
+           xTest++;
+         }
 
         //on change de joueur
         if (this.joueurEnCours == this.joueur1) {
