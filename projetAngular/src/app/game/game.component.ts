8cc254b5dc4e83362ff78b8384398fd2282dc3ab
Sylvain LE CROM <sylvain.lecrom@free.fr>
2017-10-06 08:35:22 +0200

    push grille de jeu in firebase

--- projetAngular/src/app/game/game.component.ts
+++ projetAngular/src/app/game/game.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+import { Observable } from 'rxjs/Observable';
+
+
 
 @Component({
   selector: 'app-game',
@@ -6,13 +12,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game.component.css']
 })
 export class GameComponent implements OnInit {
-
+  grille: FirebaseListObservable<any[]>;
   public items: string[][];
+  send: string;
   public couleurJoueur: string;
-  constructor() {
+  constructor(public af: AngularFireDatabase) {
+  this.grille = af.list('/grille');
     this.couleurJoueur = "jaune";
     this.items = [
-      ["vide", "vide", "vide", "vide", "vide", "vide","ghost this.couleurJoueur"],
+      ["vide", "vide", "vide", "vide", "vide", "vide", "ghost this.couleurJoueur"],
       ["vide", "vide", "vide", "vide", "vide", "vide", "ghost this.couleurJoueur"],
       ["vide", "vide", "vide", "vide", "vide", "vide", "ghost this.couleurJoueur"],
       ["vide", "vide", "vide", "vide", "vide", "vide", "ghost this.couleurJoueur"],
@@ -21,12 +29,13 @@ export class GameComponent implements OnInit {
       ["vide", "vide", "vide", "vide", "vide", "vide", "ghost this.couleurJoueur"]
     ];
     this.items[4][0] = "jaune";
+    this.send = this.items[4][2];
   }
   ngOnInit() {
+   this.grille.push({ cases :this.items });
   }
   onClick(event: Event) {
-    
-   
-    alert("click sur colonne "+ event.target + " marche");
+    alert("click sur colonne " + event.target + " marche");
   }
+
 }
