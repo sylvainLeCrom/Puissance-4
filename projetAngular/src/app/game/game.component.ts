6dc9e253065565dcc48bcec5ac861ed3e185fc7d
Matthieu Marais <mqtthieu@gmail.com>
2017-10-06 16:48:11 +0200

    tests gagnants ok + grille Full Stop

--- projetAngular/src/app/game/game.component.ts
+++ projetAngular/src/app/game/game.component.ts
@@ -7,22 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
 
+  public coupsJoués: number;
   public grille: string[][];
   public joueurEnCours: string;
   public joueur1: string;
   public joueur2: string;
   constructor() {
+    this.coupsJoués = 0;
     this.joueur1 = "rouge";
     this.joueur2 = "jaune"
     this.joueurEnCours = this.joueur1;
     this.grille = [
-      ["vide", "vide", "vide", "vide", "vide", "vide", "ghost this.couleurJoueur"],
-      ["vide", "vide", "vide", "vide", "vide", "vide", "ghost this.couleurJoueur"],
-      ["vide", "vide", "vide", "vide", "vide", "vide", "ghost this.couleurJoueur"],
-      ["vide", "vide", "vide", "vide", "vide", "vide", "ghost this.couleurJoueur"],
-      ["vide", "vide", "vide", "vide", "vide", "vide", "ghost this.couleurJoueur"],
-      ["vide", "vide", "vide", "vide", "vide", "vide", "ghost this.couleurJoueur"],
-      ["vide", "vide", "vide", "vide", "vide", "vide", "ghost this.couleurJoueur"]
+      ["vide", "vide", "vide", "vide", "vide", "vide", "ghost"+ this.joueurEnCours],
+      ["vide", "vide", "vide", "vide", "vide", "vide", "ghost"+ this.joueurEnCours],
+      ["vide", "vide", "vide", "vide", "vide", "vide", "ghost" + this.joueurEnCours],
+      ["vide", "vide", "vide", "vide", "vide", "vide", "ghost" + this.joueurEnCours],
+      ["vide", "vide", "vide", "vide", "vide", "vide", "ghost" + this.joueurEnCours],
+      ["vide", "vide", "vide", "vide", "vide", "vide", "ghost" + this.joueurEnCours],
+      ["vide", "vide", "vide", "vide", "vide", "vide", "ghost" + this.joueurEnCours]
     ];
 
   }
@@ -30,21 +32,29 @@ export class GameComponent implements OnInit {
 
   }
   clickedColumn(id: number): void {
+
+
     let x = id;
     let y = 6;
     while (y > 0) {
       if (this.grille[x][y] == 'vide') {
         this.grille[x][y] = this.joueurEnCours;
 
+        this.coupsJoués++;
+        console.log(this.coupsJoués);
+        if (this.coupsJoués == 42) {
+          alert("Draw");
+        }
+
         //on test si victoire verticale
         let xTest = x;
         let yTest = y;
         let align = 0;
-        while (yTest <= (y+3) && yTest <= 6) {
+        while (yTest <= (y + 3) && yTest <= 6) {
           if (this.grille[xTest][yTest] == this.joueurEnCours) {
             align = align + 1;
             if (align == 4) {
-              alert(this.joueurEnCours + " gagne Wouhouuuu");
+              console.log(this.joueurEnCours + " gagne Wouhouuuu");
               return;
             };
           } else {
@@ -54,29 +64,83 @@ export class GameComponent implements OnInit {
         }
 
         //on test si victoire horizontale
-         xTest = x;
-         yTest = y;
-         align = 0;
-         let recule = 3;
- 
-         //on cherche la case de départ du test
-         while (xTest > 0 && recule > 0) {
-           xTest--;
-           recule--;
-         }
-         //on check the lign
-         while (xTest <= (x+3) && xTest <= 6) {
-           if (this.grille[xTest][yTest] == this.joueurEnCours) {
-             align = align + 1;
-             if (align == 4) {
-               alert(this.joueurEnCours + " gagne Wouhouuuu");
-               return;
-             };
-           } else {
-             align = 0;
-           };
-           xTest++;
-         }
+        xTest = x;
+        yTest = y;
+        align = 0;
+        let recule = 3;
+
+        //on cherche la case de départ du test
+        while (xTest > 0 && recule > 0) {
+          xTest--;
+          recule--;
+        }
+        //on check the lign
+        while (xTest <= (x + 3) && xTest <= 6) {
+          if (this.grille[xTest][yTest] == this.joueurEnCours) {
+            align = align + 1;
+            if (align == 4) {
+              console.log(this.joueurEnCours + " gagne Wouhouuuu");
+              return;
+            };
+          } else {
+            align = 0;
+          };
+          xTest++;
+        }
+
+        //on check les diagos
+        xTest = x;
+        yTest = y;
+        align = 0;
+        recule = 3;
+
+        //on cherche la case de départ du test
+        while (xTest > 0 && yTest < 6 && recule > 0) {
+          xTest--;
+          yTest++;
+          recule--;
+        }
+        //on check the lign diago
+        while (xTest <= (x + 3) && xTest <= 6 && yTest >= (y - 3) && yTest > 1) {
+          if (this.grille[xTest][yTest] == this.joueurEnCours) {
+            align = align + 1;
+            if (align == 4) {
+              console.log(this.joueurEnCours + " gagne Wouhouuuu");
+              return;
+            };
+          } else {
+            align = 0;
+          };
+          xTest++;
+          yTest--;
+        }
+
+        //on check l'anti diago
+        xTest = x;
+        yTest = y;
+        align = 0;
+        recule = 3;
+
+        //on cherche la case de départ du test
+        while (xTest > 0 && yTest > 1 && recule > 0) {
+          xTest--;
+          yTest--;
+          recule--;
+        }
+        //on check the lign antidiago
+        while (xTest <= (x + 3) && xTest <= 6 && yTest <= (y + 3) && yTest <= 6) {
+          if (this.grille[xTest][yTest] == this.joueurEnCours) {
+            align = align + 1;
+            if (align == 4) {
+              console.log(this.joueurEnCours + " gagne Wouhouuuu");
+              return;
+            };
+          } else {
+            align = 0;
+          };
+          xTest++;
+          yTest++;
+        }
 
         //on change de joueur
         if (this.joueurEnCours == this.joueur1) {
@@ -95,7 +159,7 @@ export class GameComponent implements OnInit {
       };
 
     }
-    alert("Full");
+    console.log("Full");
     return;
   }
 
