ed114ccdc39e0a5788340e4eca4f4e49e4f6043c
Matthieu Marais <mqtthieu@gmail.com>
2017-10-06 11:08:06 +0200

    pose pions test vertical et changement joueur

--- projetAngular/src/app/game/game.component.ts
+++ projetAngular/src/app/game/game.component.ts
@@ -7,11 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
 
-  public items: string[][];
-  public couleurJoueur: string;
+  public grille: string[][];
+  public joueurEnCours: string;
+  public joueur1: string;
+  public joueur2: string;
   constructor() {
-    this.couleurJoueur = "jaune";
-    this.items = [
+    this.joueur1 = "rouge";
+    this.joueur2 = "jaune"
+    this.joueurEnCours = this.joueur1;
+    this.grille = [
       ["vide", "vide", "vide", "vide", "vide", "vide", "ghost this.couleurJoueur"],
       ["vide", "vide", "vide", "vide", "vide", "vide", "ghost this.couleurJoueur"],
       ["vide", "vide", "vide", "vide", "vide", "vide", "ghost this.couleurJoueur"],
@@ -25,14 +29,56 @@ export class GameComponent implements OnInit {
   ngOnInit() {
 
   }
-
   clickedColumn(id: number) {
-    alert(id);
+    let x = id;
+    let y = 6;
+    while (y > 0) {
+      if (this.grille[x][y] == 'vide') {
+        this.grille[x][y] = this.joueurEnCours;
+
+        //on test si victoire verticale
+        let align = 0;
+        while (y <= 6) {
+          if (this.grille[x][y] == this.joueurEnCours) {
+            align = align+1;
+            console.log(align);
+            if (align == 4) {
+              alert(this.joueurEnCours + " gagne Wouhouuuu");
+              return;
+            };
+          } else {
+            return;
+          };
+          y++;
+        }
+
+
+        //on change de joueur
+        if (this.joueurEnCours == this.joueur1) {
+          this.joueurEnCours = this.joueur2;
+        } else {
+          this.joueurEnCours = this.joueur1;
+        }
+
+
+
+
+        return;
+      } else {
+        y--;
+
+      };
+
+    }
+    alert("Full");
+    return;
   }
 
-  /*onClick(event: Event) {
-    
-   
-    alert("click sur colonne "+ event.target + " marche");
-  }*/
 }
+
+
+
+
+
+
+
