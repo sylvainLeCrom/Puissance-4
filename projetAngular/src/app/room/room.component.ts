1a8854796e2586f47980eb6f8d8481708e526668
Sylvain LE CROM <sylvain.lecrom@free.fr>
2017-10-12 00:34:35 +0200

    gestion des infos de joueur dans la room

--- projetAngular/src/app/room/room.component.ts
+++ projetAngular/src/app/room/room.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+import { Observable } from 'rxjs/Observable';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-room',
@@ -7,9 +12,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RoomComponent implements OnInit {
 
-  constructor() { }
+  public userUID: any;
+  public indexRoom: number;
+  public IDJoueur: string;
+  public couleurJoueur: string;
+  public indexJoueur: number;
+  public pseudo: string;
+
+  constructor(public af: AngularFireDatabase, private authService: AuthService) {
+
+
+
+  }
 
   ngOnInit() {
+    this.authService.authState.subscribe((userAuth) => {
+      this.userUID = userAuth.uid.toString();
+      const userPath = "users/" + this.userUID;
+      this.af.object(userPath).subscribe((user) => {
+        this.IDJoueur = user.IDduJoueur;
+        this.couleurJoueur = user.couleur;
+        this.indexJoueur = user.index;
+        this.indexRoom = user.indexRoom;
+        this.pseudo = user.pseudo;
+        while (this.indexRoom == undefined) {
+        }
+      }
+      );
+    });
   }
 
 }
