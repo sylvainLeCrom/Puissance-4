1c4b219594cad40483eecc3cb971ddad1c7a3e84
Sylvain LE CROM <sylvain.lecrom@free.fr>
2017-10-18 08:47:38 +0200

    correction routing

--- projetAngular/src/app/app-routing.ts
+++ projetAngular/src/app/app-routing.ts
@@ -3,7 +3,6 @@ import { Routes } from '@angular/router';
 import { PseudoComponent } from './pseudo/pseudo.component';
 import { RoomComponent } from './classic/room/room.component';
 import { RoomWoodComponent } from './wood/roomWood/roomWood.component';
-
 import { LoginComponent } from './login/login.component';
 //import { ChatComponent } from './chat/chat.component';
 
