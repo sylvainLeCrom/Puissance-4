4782410d6ca12ce331ca25972d2a42266d75ed12
Sylvain LE CROM <sylvain.lecrom@free.fr>
2017-10-18 18:12:02 +0200

    résolution problème rooms secondaires

--- projetAngular/src/app/pseudo/pseudo.component.ts
+++ projetAngular/src/app/pseudo/pseudo.component.ts
@@ -16,11 +16,8 @@ export class PseudoComponent implements OnInit {
 
   public maxJoueur: number;
   public nbJoueur: number;
-  public roomsArray: any;
   public numeroRoom: number;
   public pseudo: string;
-  public TotalRoom: FirebaseObjectObservable<any>;
-  public nbRoom: number;
   public gamers: FirebaseObjectObservable<any[]>;
   public gamer: any;
   public joueur1: string;
@@ -35,10 +32,7 @@ export class PseudoComponent implements OnInit {
     this.pseudo = '';
     this.joueur1 = "rouge";
     this.joueur2 = "jaune";
-    this.roomsArray;
     this.theme = "classic";
-    this.TotalRoom = af.object('/' + this.theme + '/numberOpenRoom', { preserveSnapshot: true });
-
 
   };
   ngOnInit() {
@@ -47,18 +41,9 @@ export class PseudoComponent implements OnInit {
       const userPath = "users/" + this.userUID;
       this.af.object(userPath).subscribe((user) => {
         this.pseudo = user.pseudo;
-        console.log(this.pseudo);
       });
-
     });
-
     this.nbJoueur = 0;
-    this.TotalRoom.subscribe(snapshot => {
-      this.nbRoom = snapshot.val();
-    });
-    this.af.object("/" + this.theme + "/rooms").subscribe((rooms) => {
-      this.roomsArray = rooms;
-    });
   };
   themeInf() {
     if (this.theme == "classic") {
@@ -79,99 +64,110 @@ export class PseudoComponent implements OnInit {
     this.afAuth.auth.signOut();
   }
   redirectToRoom(input) {
+    this.af.object("/" + this.theme + "/rooms").take(1).subscribe((rooms) => {
 
-    if (this.nbRoom == 0) {
-      //on crée la première room
-      this.nbRoom = 1;
-      this.numeroRoom = new Date().valueOf();
-      this.af.object("/" + this.theme + "/").set({ numberOpenRoom: this.nbRoom, rooms: this.numeroRoom });
-      //on tire au sort le numéro et la couleur du premier Gamer dans la room
-      let random = Math.floor(Math.random() * 2) + 1;
-      if (random == 1) {
-        this.couleur = this.joueur1;
-      } else {
-        this.couleur = this.joueur2;
-      }
-      //on change le nb de joueur dans la room
-      this.nbJoueur = 1;
-      this.af.object("/" + this.theme + "/rooms/0").set({ room: this.numeroRoom, nbJoueur: this.nbJoueur, placement: random });
-      //on récupère les infos du tout premier joueur      
-      this.pseudo = input;
-      let ID = Math.floor(Math.random() * 100) + 1;
-      this.gamers = this.af.object("/" + this.theme + "/rooms/0/gamers/joueur" + random);
-      this.gamer = { IDduJoueur: this.pseudo + ID, pseudo: this.pseudo, couleur: this.couleur, index: ID };
-      this.gamers.set(this.gamer);
+      this.af.object('/' + this.theme)
+        .take(1)
+        .subscribe((themeRooms) => {
+          let nbRoom = themeRooms.numberOpenRoom;
+          console.log(nbRoom);
 
-      this.af.object("users/" + this.userUID).update({ indexRoom: 0, theme: this.theme, IDduJoueur: this.pseudo + ID, pseudo: this.pseudo, couleur: this.couleur, index: ID });
-      if (this.theme == "wood") {
-        this.router.navigateByUrl('/roomWood');
-      } else {
-        this.router.navigateByUrl('/room');
-      }
+          if (nbRoom == 0) {
+            console.log("FIRST");
+            //on crée la première room
+            nbRoom = 1;
+            this.numeroRoom = new Date().valueOf();
+            this.af.object("/" + this.theme + "/").set({ numberOpenRoom: nbRoom, rooms: this.numeroRoom });
+            //on tire au sort le numéro et la couleur du premier Gamer dans la room
+            let random = Math.floor(Math.random() * 2) + 1;
+            if (random == 1) {
+              this.couleur = this.joueur1;
+            } else {
+              this.couleur = this.joueur2;
+            }
+            //on change le nb de joueur dans la room
+            this.nbJoueur = 1;
+            this.af.object("/" + this.theme + "/rooms/0").set({ room: this.numeroRoom, nbJoueur: this.nbJoueur, placement: random });
+            //on récupère les infos du tout premier joueur      
+            this.pseudo = input;
+            let ID = Math.floor(Math.random() * 100) + 1;
+            this.gamers = this.af.object("/" + this.theme + "/rooms/0/gamers/joueur" + random);
+            this.gamer = { IDduJoueur: this.pseudo + ID, pseudo: this.pseudo, couleur: this.couleur, index: ID };
+            this.gamers.set(this.gamer);
 
-    } else {
-      let index = 0
-      while (index < this.nbRoom) {
-        if (this.roomsArray[index].nbJoueur == 1) {
-          this.nbJoueur = 2;
-          this.af.object("/" + this.theme + "/rooms/" + index).update({ nbJoueur: this.nbJoueur });
+            this.af.object("users/" + this.userUID).update({ indexRoom: 0, theme: this.theme, IDduJoueur: this.pseudo + ID, pseudo: this.pseudo, couleur: this.couleur, index: ID });
+            if (this.theme == "wood") {
+              this.router.navigateByUrl('/roomWood');
+            } else {
+              this.router.navigateByUrl('/room');
+            }
 
-          //on récupère les infos du deuxième joueur dans cette room    
-          this.pseudo = input;
-          let ID = Math.floor(Math.random() * 100) + 1;
-          if (this.roomsArray[index].placement == 1) {
-            var lastplace = 2;
-            this.couleur = this.joueur2;
           } else {
-            lastplace = 1;
-            this.couleur = this.joueur1;
+            let index = 0
+            while (index < nbRoom) {
+              if (rooms[index].nbJoueur == 1) {
+                this.nbJoueur = 2;
+                this.af.object("/" + this.theme + "/rooms/" + index).update({ nbJoueur: this.nbJoueur });
+
+                //on récupère les infos du deuxième joueur dans cette room    
+                this.pseudo = input;
+                let ID = Math.floor(Math.random() * 100) + 1;
+                if (rooms[index].placement == 1) {
+                  var lastplace = 2;
+                  this.couleur = this.joueur2;
+                } else {
+                  lastplace = 1;
+                  this.couleur = this.joueur1;
+                }
+                this.af.object("/" + this.theme + "/rooms/" + index).update({ placement: "full" })
+
+                this.gamers = this.af.object("/" + this.theme + "/rooms/" + index + "/gamers/joueur" + lastplace);
+                this.gamer = { IDduJoueur: this.pseudo + ID, pseudo: this.pseudo, couleur: this.couleur, index: ID };
+                this.gamers.set(this.gamer);
+                this.af.object("users/" + this.userUID).update({ indexRoom: index, theme: this.theme, IDduJoueur: this.pseudo + ID, pseudo: this.pseudo, couleur: this.couleur, index: ID });
+                if (this.theme == "wood") {
+                  this.router.navigateByUrl('/roomWood');
+                } else {
+                  this.router.navigateByUrl('/room');
+                }
+                return;
+              } else {
+                console.log("cette room est pleine" + index);
+              }
+              index++;
+            }
+            nbRoom++;
+            this.numeroRoom = new Date().valueOf();
+            console.log(rooms);
+            rooms.push("room" + this.numeroRoom);
+            this.af.object("/" + this.theme + "/").update({ numberOpenRoom: nbRoom });
+
+            //on tire au sort le numéro et la couleur du premier Gamer dans la room
+            let random = Math.floor(Math.random() * 2) + 1;
+            if (random == 1) {
+              this.couleur = this.joueur1;
+            } else {
+              this.couleur = this.joueur2;
+            }
+            //on change le nb de joueur dans la room
+            this.nbJoueur = 1;
+            this.af.object("/" + this.theme + "/rooms/" + index).set({ room: this.numeroRoom, nbJoueur: this.nbJoueur, placement: random });
+            //on récupère les infos du joueur      
+            this.pseudo = input;
+            let ID = Math.floor(Math.random() * 100) + 1;
+            this.gamers = this.af.object("/" + this.theme + "/rooms/" + index + "/gamers/joueur" + random);
+            this.gamer = { IDduJoueur: this.pseudo + ID, pseudo: this.pseudo, couleur: this.couleur, index: ID };
+            this.gamers.set(this.gamer);
+
+            this.af.object("users/" + this.userUID).update({ indexRoom: index, theme: this.theme, IDduJoueur: this.pseudo + ID, pseudo: this.pseudo, couleur: this.couleur, index: ID });
+            if (this.theme == "wood") {
+              this.router.navigateByUrl('/roomWood');
+            } else {
+              this.router.navigateByUrl('/room');
+            } return;
           }
-          this.af.object("/" + this.theme + "/rooms/" + index).update({ placement: "full" })
-
-          this.gamers = this.af.object("/" + this.theme + "/rooms/" + index + "/gamers/joueur" + lastplace);
-          this.gamer = { IDduJoueur: this.pseudo + ID, pseudo: this.pseudo, couleur: this.couleur, index: ID };
-          this.gamers.set(this.gamer);
-          this.af.object("users/" + this.userUID).update({ indexRoom: index, theme: this.theme, IDduJoueur: this.pseudo + ID, pseudo: this.pseudo, couleur: this.couleur, index: ID });
-          if (this.theme == "wood") {
-            this.router.navigateByUrl('/roomWood');
-          } else {
-            this.router.navigateByUrl('/room');
-          }
-          return;
-        } else {
-          console.log("cette room est pleine" + index);
-        }
-        index++;
-      }
-      this.nbRoom++;
-      this.numeroRoom = new Date().valueOf();
-      this.roomsArray.push("room" + this.numeroRoom);
-      this.af.object("/" + this.theme + "/").update({ numberOpenRoom: this.nbRoom });
-
-      //on tire au sort le numéro et la couleur du premier Gamer dans la room
-      let random = Math.floor(Math.random() * 2) + 1;
-      if (random == 1) {
-        this.couleur = this.joueur1;
-      } else {
-        this.couleur = this.joueur2;
-      }
-      //on change le nb de joueur dans la room
-      this.nbJoueur = 1;
-      this.af.object("/" + this.theme + "/rooms/" + index).set({ room: this.numeroRoom, nbJoueur: this.nbJoueur, placement: random });
-      //on récupère les infos du joueur      
-      this.pseudo = input;
-      let ID = Math.floor(Math.random() * 100) + 1;
-      this.gamers = this.af.object("/" + this.theme + "/rooms/" + index + "/gamers/joueur" + random);
-      this.gamer = { IDduJoueur: this.pseudo + ID, pseudo: this.pseudo, couleur: this.couleur, index: ID };
-      this.gamers.set(this.gamer);
-
-      this.af.object("users/" + this.userUID).update({ indexRoom: index, theme: this.theme, IDduJoueur: this.pseudo + ID, pseudo: this.pseudo, couleur: this.couleur, index: ID });
-      if (this.theme == "wood") {
-        this.router.navigateByUrl('/roomWood');
-      } else {
-        this.router.navigateByUrl('/room');
-      }      return;
-    }
+        });
+    });
   }
 }
 
